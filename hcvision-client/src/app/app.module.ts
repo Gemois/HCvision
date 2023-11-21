62f517f7663c638686ade91e6db2319a8f39a3b3
Giorgos Moisidis <37257368+Gemois@users.noreply.github.com>
2023-11-21 21:06:56 +0200

    Add base project structure
    Added base auto-generated components with Angular CLI

--- hcvision-client/src/app/app.module.ts
+++ hcvision-client/src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HistoryComponent } from './components/history/history.component';
+import { HierarchicalComponent } from './components/hierarchical/hierarchical.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { DatasetComponent } from './components/dataset/dataset.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    AboutComponent,
+    HistoryComponent,
+    HierarchicalComponent,
+    ProfileComponent,
+    DatasetComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ hcvision-client/src/app/components/hierarchical/hierarchical.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hierarchical',
+  templateUrl: './hierarchical.component.html',
+  styleUrl: './hierarchical.component.css'
+})
+export class HierarchicalComponent {
+
+}
